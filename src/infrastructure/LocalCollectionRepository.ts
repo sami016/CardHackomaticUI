71826c9bd3ee643fd327bb5efa06231e63f7e1be
Sam Holder <samholder@live.co.uk>
2024-01-11 00:01:24 +0000

    prevented storing in local storage due to image size problems. Increased card size.

--- src/infrastructure/LocalCollectionRepository.ts
+++ src/infrastructure/LocalCollectionRepository.ts
@@ -5,14 +5,15 @@ const collectionsEntryName = 'collections'
 
 export class LocalCollectionRepository implements ICollectionRepository {
     writeAll(collections: ICollection[]): void {
-        localStorage.setItem(collectionsEntryName, JSON.stringify(collections))
+        //localStorage.setItem(collectionsEntryName, JSON.stringify(collections))
     }
 
     readAll(): ICollection[] {
-        const existingValue = localStorage.getItem(collectionsEntryName)
-        if (existingValue === null) {
-            return []
-        }
-        return JSON.parse(existingValue)
+        return [];
+        // const existingValue = localStorage.getItem(collectionsEntryName)
+        // if (existingValue === null) {
+        //     return []
+        // }
+        // return JSON.parse(existingValue)
     }
 }
