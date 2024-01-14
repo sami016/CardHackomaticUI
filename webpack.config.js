feb3631d7768e57482e2fce17d1e5cd37bba1815
Sam Holder <samholder@live.co.uk>
2024-01-14 14:11:43 +0000

    fix public path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8402/",
+    publicPath: "/",
   },
 
   resolve: {
